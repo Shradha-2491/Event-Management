f9d5f7ac96597868188458c7fa3a2a2067e9bcbb
Shradha Suryawanshi <78255973+Shradha-2491@users.noreply.github.com>
2025-02-08 16:08:50 +0530

    Update backend url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_API_URL, {
+const socket = io(process.env.REACT_APP_API_URL || "https://event-management-server-v6v9.onrender.com", {
     autoConnect: false,
 });
 
